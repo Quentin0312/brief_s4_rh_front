ef269604fd40e88eb52e3fb889653b154dfb320e
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-10 14:25:46 +0400

    wip: setup solidJS project

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar(page: any) {
+  return (
+    <nav
+      class="w-full text-center text-xl flex justify-center items-center relative"
+      style="height: 64px"
+    >
+      <a href="/" class="text-center text-black tracking-[.20em]">
+        My RH Solution
+      </a>
+    </nav>
+  );
+}
---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+export const Formater = (data: any) => {
+  const formdata = new FormData();
+  Object.keys(data).forEach((_data) => formdata.append(_data, data[_data]));
+  return formdata;
+};
+
+export const request = async (url: string, method: string, data: any) => {
+  const host = "http://localhost:8000/";
+  return fetch(host + url, { method: method, body: data }); // , headers: {Authorization: String(token)}
+};
