e46db359d77615061dd53c4e5245f770c31f94f9
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-14 21:21:49 +0400

    eslint almost completely fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,32 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    // "plugin:prettier/recommended",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    parser: "@typescript-eslint/parser",
-    plugins: ["solid", "@typescript-eslint/eslint-plugin"],
-    extends: [
-      "plugin:solid/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:prettier/recommended",
-    ],
-    ignorePatterns: [".eslintrc.js", "node_modules", "dist"],
-    overrides: [],
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    rules: {
-      "solid/reactivity": "warn",
-      "solid/no-destructure": "warn",
-      "solid/jsx-no-undef": "error",
-      quotes: "error",
-      "@typescript-eslint/no-explicit-any": "error",
-    },
-  };
-  
+  },
+  "plugins": [
+    "@typescript-eslint", "solid"
+  ],
+  "rules": {
+    "solid/reactivity": "warn",
+    "solid/no-destructure": "warn",
+    "solid/jsx-no-undef": "error",
+    quotes: "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+  ignorePatterns: [".eslintrc.js", "node_modules", "dist","*.config.*"],
+  overrides: [],
+};
+
