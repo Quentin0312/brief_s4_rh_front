3cfcbbfb84e1b76031225d38ade96b05b7e11eca
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-15 21:44:47 +0400

    wip: navbar style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,18 +3,20 @@ import { deselectEmployee, employees } from "./EmployeeGrid";
 export default function Navbar() {
   return (
     <nav
-      class="w-full text-center text-xl flex justify-center items-center relative"
+      class="w-full text-center text-xl flex justify-center items-center relative pt-10 pb-24"
       style="height: 64px"
     >
       <button
-        class="btn btn-outline absolute left-0"
+        class="btn btn-outline absolute left-10"
         onClick={() => {
           deselectEmployee(employees());
         }}
       >
         Ajouter un employé
       </button>
-      <a class="text-center text-black tracking-[.20em]">My RH Solution</a>
+      <a class="text-center text-xl text-slate-300 tracking-[.40em]">
+        My RH Solution
+      </a>
     </nav>
   );
 }
