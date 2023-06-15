c0c142221c5fb795ef3510bc469b328b4304bbe5
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-15 21:37:21 +0400

    addEmployee button puted into navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { type Component, Switch, Match } from "solid-js";
 import Navbar from "./components/navbar";
-import EmployeeGrid, {
-  deselectEmployee,
-  employees,
-} from "./components/EmployeeGrid";
+import EmployeeGrid from "./components/EmployeeGrid";
 import EmployeeInfo, { getSelectedEmployee } from "./components/EmployeeInfo";
 import AddEmployee from "./components/AddEmployee";
 import ModifyEmployee, {
@@ -14,15 +11,6 @@ const App: Component = () => {
   return (
     <>
       <Navbar />
-
-      <button
-        class="btn btn-neutral"
-        onClick={() => {
-          deselectEmployee(employees());
-        }}
-      >
-        Ajouter un employé
-      </button>
 
       <div class="grid grid-cols-2">
         <div class="flex justify-center">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,19 @@
+import { deselectEmployee, employees } from "./EmployeeGrid";
+
 export default function Navbar() {
   return (
     <nav
       class="w-full text-center text-xl flex justify-center items-center relative"
       style="height: 64px"
     >
+      <button
+        class="btn btn-outline absolute left-0"
+        onClick={() => {
+          deselectEmployee(employees());
+        }}
+      >
+        Ajouter un employé
+      </button>
       <a class="text-center text-black tracking-[.20em]">My RH Solution</a>
     </nav>
   );
